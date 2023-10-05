dc20444ed97c4c6983fbc35e2227d4dec0a24ef4
Yagneshtarsariya <119489703+Yagneshtarsariya@users.noreply.github.com>
2023-10-05 11:53:42 +0530

    web-hophsee

---
+++ ALL_JAVA_SCRIPT_FILE/api.js
@@ -0,0 +1,11 @@
+// const host = 'http://localhost:8000';
+const host = 'http://192.168.90.108:8000';
+
+const admin = `${host}/admin`;
+const adminLogin = `${host}/admin/login`;
+const doctors = `${host}/doctors`;
+const patients = `${host}/patients`;
+const employees  = `${host}/employees`;
+const employeeslogin  = `${host}/employees/login`;
+const appoinments = `${host}/appoinments`;
+const users = `${host}/users`;
---
+++ ALL_JAVA_SCRIPT_FILE/line_chart.js
@@ -0,0 +1,22 @@
+// google.charts.load('current', {'packages':['corechart']});
+//       google.charts.setOnLoadCallback(drawChart);
+
+//       function drawChart() {
+//         var data = google.visualization.arrayToDataTable([
+//           ['Year', 'Sales'],
+//           ['1',  1],
+//           ['2',  7],
+//           ['3',  11],
+//           ['4',  17],
+//         ]);
+
+//         var options = {
+//           title: '',
+//           curveType: 'function',
+//           legend: { position: 'bottom' }
+//         };
+
+//         var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
+
+//         chart.draw(data, options);
+//       }
