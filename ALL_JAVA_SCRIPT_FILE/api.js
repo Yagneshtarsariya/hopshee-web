a7285ebf143d8ecf0b77f0776deac49d84cb94e2
MehulVariya <105047887+MehulVariya@users.noreply.github.com>
2023-10-05 17:09:00 +0530

    added logout

--- ALL_JAVA_SCRIPT_FILE/api.js
+++ ALL_JAVA_SCRIPT_FILE/api.js
@@ -1,5 +1,5 @@
-// const host = 'http://localhost:8000';
-const host = 'http://192.168.90.108:8000';
+const host = 'http://localhost:8000';
+//const host = 'http://192.168.90.108:8000';
 
 const admin = `${host}/admin`;
 const adminLogin = `${host}/admin/login`;
