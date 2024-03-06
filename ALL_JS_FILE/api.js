acf1ef7e54352f6fe25df2d93459b9dfd9567676
Yagneshtarsariya <119489703+Yagneshtarsariya@users.noreply.github.com>
2024-03-06 11:40:50 +0530

    new design of web

--- ALL_JS_FILE/api.js
+++ ALL_JS_FILE/api.js
@@ -1,11 +1,11 @@
-const host = 'http://localhost:8000';
-//const host = 'http://192.168.90.108:8000';
+// const host = 'http://localhost:8000';
+const host = 'https://node-hophsee.onrender.com';
 
 const admin = `${host}/admin`;
 const adminLogin = `${host}/admin/login`;
 const doctors = `${host}/doctors`;
 const patients = `${host}/patients`;
-const employees  = `${host}/employees`;
-const employeeslogin  = `${host}/employees/login`;
+const employees = `${host}/employees`;
+const employeeslogin = `${host}/employees/login`;
 const appoinments = `${host}/appoinments`;
 const users = `${host}/users`;
