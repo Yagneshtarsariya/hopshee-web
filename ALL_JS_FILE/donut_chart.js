e5990ffe29cce4ea108ada16371197cf5bd7877a
Yagneshtarsariya <119489703+Yagneshtarsariya@users.noreply.github.com>
2024-03-09 14:33:25 +0530

    cmt

--- ALL_JS_FILE/donut_chart.js
+++ ALL_JS_FILE/donut_chart.js
@@ -6,8 +6,6 @@ function drawChart() {
         ['Pending', 11],
         ['Approved', 2],
         ['Reject', 2]
-        // ['Watch TV', 2],
-        // ['Sleep', 7]
     ]);
 
     var options = {
