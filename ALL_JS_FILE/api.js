cf456f2ba217f894179781744028e12ae9cf8dba
Yagneshtarsariya <119489703+Yagneshtarsariya@users.noreply.github.com>
2024-03-11 17:18:59 +0530

    commit

--- ALL_JS_FILE/api.js
+++ ALL_JS_FILE/api.js
@@ -1,5 +1,6 @@
 // const host = 'http://localhost:8000';
-const host = 'https://crowded-galoshes-elk.cyclic.app';
+// const host = 'https://crowded-galoshes-elk.cyclic.app';
+const host = 'https://node-hophsee.onrender.com';
 
 const admin = `${host}/admin`;
 const adminLogin = `${host}/admin/login`;
