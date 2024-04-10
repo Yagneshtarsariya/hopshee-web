093c9a573b3fd95677481c600df49715781ca515
Yagneshtarsariya <119489703+Yagneshtarsariya@users.noreply.github.com>
2024-04-10 12:22:05 +0530

    complete task

--- ALL_JS_FILE/api.js
+++ ALL_JS_FILE/api.js
@@ -1,6 +1,7 @@
 // const host = 'http://localhost:8000';
 // const host = 'https://crowded-galoshes-elk.cyclic.app';
-const host = 'https://node-hophsee.onrender.com';
+// https://hophsee-backend.onrender.com/
+const host = "https://hophsee-backend.onrender.com";
 
 const admin = `${host}/admin`;
 const adminLogin = `${host}/admin/login`;
@@ -10,7 +11,8 @@ const employees = `${host}/employees`;
 const employeeslogin = `${host}/employees/login`;
 const appoinments = `${host}/appoinments`;
 const users = `${host}/users`;
-const AppoApproved = `${host}/appoinments/approved`
-const Appoupcoming = `${host}/appoinments/upcoming`
-const Appocancelled = `${host}/appoinments/cancelled`
-const Appoexpired = `${host}/appoinments/expired`
+const AppoApproved = `${host}/appoinments/approved`;
+const Appoupcoming = `${host}/appoinments/upcoming`;
+const Appocancelled = `${host}/appoinments/cancelled`;
+const Appoexpired = `${host}/appoinments/expired`;
+const payments = `${host}/payments`;
